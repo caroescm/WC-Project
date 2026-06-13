6ee83e9bc51870d5fd85699905bddee92232889e
caroescm <caro.escudero.manrique@gmail.com>
2026-06-12 22:05:17 -0500

    data matching between new csv and current elos

--- src/config.py
+++ src/config.py
@@ -7,3 +7,15 @@ K_FACTORS = {
     "OTHER": 20,
     "FRIENDLY": 12
 }
+
+
+name_map = {
+    'Congo DR': 'DR Congo',
+    'IR Iran': 'Iran',
+    'Korea Republic': 'South Korea',
+    'Czechia': 'Czech Republic',
+    'Türkiye': 'Turkey',
+    'USA': 'United States',
+    "Côte d'Ivoire": 'Ivory Coast',
+    'Cabo Verde': 'Cape Verde',
+}
